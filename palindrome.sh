878b839d7e39dbc407bebdfca538e2ed7254f314
Afnasahal <144669637+Afnasahal@users.noreply.github.com>
2024-02-29 14:20:09 +0530

    Add files via upload

---
+++ palindrome.sh
@@ -0,0 +1,12 @@
+echo "Afna Assis
+23mca005"
+echo Enter the string
+read s
+echo $s>temp
+rvs="$(rev temp)"
+if [ $s = $rvs ]
+then
+echo "it is palindrome"
+else
+echo " it is not a Palindrome"
+fi
---
+++ reverse.sh
@@ -0,0 +1,9 @@
+echo "Afna Assis
+23mca005"
+read num
+reverse=$(echo "$num" | rev)
+if [ "$num" -eq "$reverse" ]; then
+echo "$num is same when reversed."
+else
+echo "$num is not same when reversed."
+fi
